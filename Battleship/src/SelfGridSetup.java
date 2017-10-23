45a1b926f40184e3e1d7e03c04e7800098cb98c6
anis <anisdhapa11@gmail.com>
2017-10-23 11:09:06 -0700

    add PlayerData class

---
+++ Battleship/src/SelfGridSetup.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+
+public class SelfGridSetup extends BattleGrid {
+	public SelfGridSetup(String name, BattleShip bt)
+	{
+		super(bt);
+		
+	}
+
+	    @Override
+	    protected JPanel getCell(int i ,int j, BattleShip bt)
+	    {
+	        JPanel panel = new JPanel();
+	        panel.setBackground(Color.black);
+	        panel.setBorder(BorderFactory.createLineBorder(Color.blue, 5));
+	        panel.setPreferredSize(new Dimension(20, 20)); // for demo purposes only
+	        panel.addMouseListener(new MouseAdapter() {
+	            @Override
+	            public void mouseClicked(MouseEvent e) {
+	            	
+	            }
+	        });
+	        return panel;
+	    }
+}
