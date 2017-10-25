9e3fb1d68b67e76d3ff2fb77510d2ecc68766224
Sai Sathyanantha <saisathyanantha@gmail.com>
2017-10-25 15:50:07 -0700

    Worked on Attack Grid

--- Battleship/src/AttackGrid.java
+++ Battleship/src/AttackGrid.java
@@ -18,6 +18,8 @@ public class AttackGrid extends BattleGrid {
     {
         JPanel panel = new JPanel();
         panel.setBackground(Color.white);
+
+        
         panel.setBorder(BorderFactory.createLineBorder(Color.red, 5));
         panel.setPreferredSize(new Dimension(20, 20)); // for demo purposes only
 
