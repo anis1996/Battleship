1a1fa5ed7ff17ce745c4f86bb8b5d1bd2b836645
Jean Marcellin <marcellinja@gmail.com>
2017-10-25 15:01:44 -0700

    - removed grid setup class typos

--- Battleship/src/Player1SetupState.java
+++ Battleship/src/Player1SetupState.java
@@ -3,8 +3,10 @@ public class Player1SetupState implements GameState {
 	public void drawFrame(BattleShip bs)
 	{
 		bs.setState(new Player2SetupState());
-		SelfGridSetup sgp = new SelfGridSetup("Player1", bs.Player1);
+		SelfGrid sgs = new SelfGrid("Player1", bs.Player1, false);
 		AttackGrid ag = new AttackGrid("player1", bs.Player1);
-		PlayerScreen player1 = new PlayerScreen("Player1", true, bs, sgp, ag);
+		PlayerScreen player1 = new PlayerScreen("Player1", true, bs, sgs, ag);
 	}
+
+    public void drawDefenseFrame(){}
 }
--- Battleship/src/Player2SetupState.java
+++ Battleship/src/Player2SetupState.java
@@ -4,7 +4,7 @@ public class Player2SetupState implements GameState{
 	public void drawFrame(BattleShip bs)
 	{
 		bs.setState(new Player1AttackState());
-		SelfGridSetup sgp = new SelfGridSetup("Player1", bs.Player2);
+		SelfGrid sgp = new SelfGrid("Player1", bs.Player2, false);
 		AttackGrid ag = new AttackGrid("player1", bs.Player2);
 		PlayerScreen player2 = new PlayerScreen("Player2", true,bs,sgp,ag);
 
